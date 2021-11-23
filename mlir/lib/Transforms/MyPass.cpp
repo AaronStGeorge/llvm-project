2a9c638ce4a8403f966933ed30e912733b02373c
AaronStGeorge <aaronstgeorge@gmail.com>
2021-11-23 11:21:30 -0700

    Minimum Viable Gradable Artifact

--- mlir/lib/Transforms/MyPass.cpp
+++ mlir/lib/Transforms/MyPass.cpp
@@ -1,7 +1,10 @@
 #include "PassDetail.h"
-#include "mlir/Transforms/Passes.h"
-
+#include "mlir/Analysis/Utils.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/Transforms/LoopFusionUtils.h"
+#include "mlir/Transforms/Passes.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/Support/CommandLine.h"
 
 #define DEBUG_TYPE "my-pass"
@@ -9,25 +12,81 @@
 using namespace mlir;
 
 namespace {
-    /// Loop invariant code motion (LICM) pass.
-    struct MyPass
-            : public MyPassBase<MyPass> {
-        void runOnFunction() override;
-    };
+/// Loop invariant code motion (LICM) pass.
+struct MyPass : public MyPassBase<MyPass> {
+  void runOnFunction() override;
+};
 } // end anonymous namespace
 
 std::unique_ptr<OperationPass<FuncOp>> mlir::createMyPass() {
-    return std::make_unique<MyPass>();
+  return std::make_unique<MyPass>();
 }
+
+namespace {
+struct Node {
+  unsigned nodeId;
+  Operation *op;
+  SmallVector<Operation *> loads;
+  SmallVector<Operation *> stores;
+
+  explicit Node(unsigned nodeId, Operation *op, SmallVector<Operation *> loads,
+                SmallVector<Operation *> stores)
+      : nodeId(nodeId), op(op), loads(loads), stores(stores) {}
+  ~Node() = default;
+  Node(const Node &other) = delete;
+  Node(Node &&other) noexcept = default;
+  Node &operator=(const Node &other) = delete;
+  Node &operator=(Node &&other) noexcept = delete;
+};
+} // namespace
 
 void MyPass::runOnFunction() {
-    LLVM_DEBUG(llvm::dbgs() << "TACO: Hello from my pass!\n");
-    FuncOp f = getOperation();
+  auto f{getFunction()};
+  DenseMap<unsigned, Node> nodes;
+  unsigned nodeId = 0;
+  for (auto &forOp : f.front()) {
+    if (!isa<AffineForOp>(forOp)) {
+      continue;
+    }
+    SmallVector<Operation *, 4> loads;
+    SmallVector<Operation *, 4> stores;
+    forOp.walk([&](Operation *op) {
+      if (isa<AffineReadOpInterface>(op)) {
+        loads.push_back(op);
+      } else if (isa<AffineWriteOpInterface>(op)) {
+        stores.push_back(op);
+      }
+    });
+    Node n{nodeId, &forOp, std::move(loads), std::move(stores)};
+    nodes.insert({nodeId, std::move(n)});
+    nodeId++;
+  }
 
-    auto a = [](Operation *inst) {
-        LLVM_DEBUG(llvm::dbgs() << "TACO: Hello from my pass!\n");
-    };
-
-    f->walk(a);
+  DenseMap<unsigned, unsigned> producerConsumerCandidates;
+  for (auto &idAndNode : nodes) {
+    unsigned srcId{idAndNode.first};
+    const Node &srcNode{idAndNode.second};
+    DenseSet<Value> memRefs;
+    for (Operation *store : srcNode.stores) {
+      memRefs.insert(cast<AffineWriteOpInterface>(store).getMemRef());
+    }
+    for (unsigned dstId = srcId + 1; dstId < nodeId; dstId++) {
+      const Node &dstNode{nodes.find(dstId)->second};
+      if (llvm::any_of(dstNode.loads, [&](Operation *op) {
+            auto loadOp = cast<AffineReadOpInterface>(op);
+            return memRefs.count(loadOp.getMemRef()) > 0;
+          })) {
+        auto srcAffineForOp = cast<AffineForOp>(srcNode.op);
+        auto dstAffineForOp = cast<AffineForOp>(dstNode.op);
+        ComputationSliceState computationSliceState = ComputationSliceState();
+        FusionResult result = mlir::canFuseLoops(
+            srcAffineForOp, dstAffineForOp, 1, &computationSliceState,
+            FusionStrategy::ProducerConsumer);
+        if (result.value == FusionResult::Success) {
+          fuseLoops(srcAffineForOp, dstAffineForOp, computationSliceState);
+          srcAffineForOp.erase();
+        }
+      }
+    }
+  }
 }
-
--- mlir/test/Transforms/my-pass.mlir
+++ mlir/test/Transforms/my-pass.mlir
@@ -1,4 +1,4 @@
-// RUN: mlir-opt -allow-unregistered-dialect %s -small-affine-loop-fusion -split-input-file | FileCheck %s
+// RUN: mlir-opt -allow-unregistered-dialect %s -my-pass -split-input-file | FileCheck %s
 
 // CHECK-LABEL: func @should_fuse_raw_dep_for_locality() {
 func @should_fuse_raw_dep_for_locality() {
@@ -36,6 +36,39 @@ func @should_not_fuse_reverse_loop() {
   // CHECK-NEXT:   affine.store %{{.*}}, %{{.*}}[%{{.*}}] : memref<10xf32>
   // CHECK-NEXT: }
   // CHECK-NEXT: affine.for %{{.*}} = 0 to 10 {
+  // CHECK-NEXT:   affine.load %{{.*}}[%{{.*}}] : memref<10xf32>
+  // CHECK-NEXT: }
+  // CHECK-NEXT: return
+  return
+}
+
+// -----
+
+// CHECK-LABEL: func @should_not_fuse_no_deps() {
+func @should_not_fuse_no_deps() {
+  %a = memref.alloc() : memref<10xf32>
+  %b = memref.alloc() : memref<10xf32>
+
+  %cf7 = constant 7.0 : f32
+
+  affine.for %i0 = 0 to 10 {
+    affine.store %cf7, %a[%i0] : memref<10xf32>
+  }
+  affine.for %i1 = 0 to 10 {
+    affine.store %cf7, %b[%i1] : memref<10xf32>
+  }
+  affine.for %i2 = 0 to 10 {
+    %v0 = affine.load %a[%i2] : memref<10xf32>
+  }
+  affine.for %i3 = 0 to 10 {
+    %v1 = affine.load %b[%i3] : memref<10xf32>
+  }
+  // CHECK:      affine.for %{{.*}} = 0 to 10 {
+  // CHECK-NEXT:   affine.store %{{.*}}, %{{.*}}[%{{.*}}] : memref<10xf32>
+  // CHECK-NEXT:   affine.load %{{.*}}[%{{.*}}] : memref<10xf32>
+  // CHECK-NEXT: }
+  // CHECK-NEXT: affine.for %{{.*}} = 0 to 10 {
+  // CHECK-NEXT:   affine.store %{{.*}}, %{{.*}}[%{{.*}}] : memref<10xf32>
   // CHECK-NEXT:   affine.load %{{.*}}[%{{.*}}] : memref<10xf32>
   // CHECK-NEXT: }
   // CHECK-NEXT: return
